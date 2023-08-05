19f35c5bfbaf5ea1f3ff9f150455c43fab56eea9
Vladyslav Sydorchuk <vlalmajep@gmail.com>
2023-08-06 02:05:09 +0300

    “description”

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function getLinks(ipfsPath) {
 }
 async function retrieve (cid) {
   const client = create({ url: "http://127.0.0.1:5001" });
-  const res = await client.get(cid)
+  const res = await client.cat(cid)
   console.log(`Got a response! [${res.status}] ${res.statusText}`)
   if (!res.ok) {
     throw new Error(`failed to get ${cid}`)
