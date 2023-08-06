6ae3275f9a4719a4d8ebf62c2058b640fb47ba82
Vladyslav Sydorchuk <vlalmajep@gmail.com>
2023-08-06 19:23:22 +0300

    “description”

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ import { create } from 'ipfs-http-client';
 // getFile11('bafzbeicnvxhpjwpnt5ju3h5mtenp3y63rl272sib6ebauutmqe2ymax36e').catch(console.error); // site
 // convertHashToCar('bafybeibrkegmkwxp46rtz63gu25exeexhbzu42gye6wqm3w3i2ok4qalpi').catch(console.error); // pepa
 
-async function retrieve (cid) {
+async function getLinks(ipfsPath) {
   const client = create({ url: "http://127.0.0.1:5001" });
-  const res = await client.get(cid)
-  console.log(`Got a response! [${res.status}] ${res.statusText}`)
-  if (!res.ok) {
-    throw new Error(`failed to get ${cid}`)
-  }
-}
 
-retrieve('bafzbeicnvxhpjwpnt5ju3h5mtenp3y63rl272sib6ebauutmqe2ymax36e')
+  const links = [];
+  for await (const link of client.ls(ipfsPath)) {
+    links.push(link);
+  }
+  console.log(links);
+}
+getLinks('bafzbeicnvxhpjwpnt5ju3h5mtenp3y63rl272sib6ebauutmqe2ymax36e')
 
 
 //   // unpack File objects from the response
