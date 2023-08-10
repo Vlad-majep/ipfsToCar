fa3eba30240ce25adb10750450d80d7d04ac44c3
Vladyslav Sydorchuk <vlalmajep@gmail.com>
2023-08-10 11:48:28 +0300

    “description”

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ import path from "path";
 import { Writable } from 'stream'
 import { createDirectoryEncoderStream, CAREncoderStream } from 'ipfs-car'
 import { filesFromPaths } from 'files-from-path'
+import { HttpJsonRpcConnector, LotusWalletProvider, LotusClient} from "filecoin.js";
 
 
+const localNodeUrl = "http://127.0.0.1:1234/rpc/v0";
+const signAuthToken ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.ro6hN4mxR_Xsv47vmkM59_5H84VmYwYY3GM9gjLW4zk"
+const httpConnector = new HttpJsonRpcConnector({ url: localNodeUrl, token: signAuthToken });
+const lotusClient = new LotusClient(httpConnector);
+const lotusWallet = new LotusWalletProvider(lotusClient);
 const client = create({ url: "http://127.0.0.1:5001" });
+const ipfsHash = 'QmW9doQsy4uMgsRrB9KpHgZDH577nHU2qfPxCEpHQK2e1s'; // Основная папка
 
-const mainFolder = 'QmW9doQsy4uMgsRrB9KpHgZDH577nHU2qfPxCEpHQK2e1s'; // Основная папка
-
-// Убедитесь, что главная папка существует
-if (!fs.existsSync(mainFolder)) {
-  fs.mkdirSync(mainFolder, { recursive: true });
-}
-
-async function getLinks(ipfsPath, localPath = mainFolder) {
+async function getLinks(ipfsPath, localPath = ipfsHash) {
+  if (!fs.existsSync(ipfsHash)) {
+    fs.mkdirSync(ipfsHash, { recursive: true });
+  }  
   for await (const link of client.ls(ipfsPath)) {
     const newPath = path.join(localPath, link.name);
     const links = [];
@@ -52,68 +55,39 @@ async function retrieve(cid, filePath) {
   writeStream.end();
 }
 
-getLinks(mainFolder).then(() => {
-  getCAr([mainFolder]);
+async function storeFile(){
+  try {
+    // 1. import data to lotus
+    const importResult = await lotusClient.client.import({
+      Path: "5gb-filecoin-huyni.bin",
+      IsCAR: false,
+  });
+  console.log(importResult);
+  const queryOffer = await lotusClient.client.minerQueryOffer('f01832584', importResult);
+  console.log(queryOffer);
+  const isActive = importResult.Root["/"] === queryOffer.Root["/"];
+  console.log("Provider is active: ", isActive);
+    //3. start storage deal with SP
+    if(isActive){
+      const dealCid = await lotusClient.client.startDeal({
+        Data: {
+          TransferType: 'graphsync',
+          Root: importResult.Root,
+        },
+        Miner: 'f01832584',
+        Wallet: await lotusWallet.getDefaultAddress(),
+        EpochPrice: queryOffer["MinPrice"],
+        MinBlocksDuration: 434000,
+      });
+
+      
+      console.log("dealCID is ", dealCid);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getLinks(ipfsHash).then(() => {
+  getCAr([ipfsHash]);
 });
-
-
-//   // unpack File objects from the response
-//   const files = await res.files()
-//   for (const file of files) {
-//     console.log(`${file.cid} -- ${file.path} -- ${file.size}`)
-//   }
-//   // request succeeded! do something with the response object here...
-// }
-
-
-
-// process.once('uncaughtException', (err, origin) => {
-//   console.error(err);
-// })
-
-  
-  // writer._mutex.then(r => {
-  //   console.log('writer._mutex', r);
-  // }).catch(e => {
-  //   console.error('writer._mutex e', e);
-  // });
-  
-
-  // await new Promise((resolve) => {
-  //   asyncIteratorToStream(bytesIterable)
-  //       .on('data', (chunk) => {
-  //         console.log('data', chunk);
-  //         writer.put({cid: cid, bytes: chunk});
-  //       })
-  //       .on('close', (chunk) => {
-  //         console.log('close');
-  //         resolve();
-  //       });
-  // });
-    // process.on('uncaughtException', (err) => {
-  //   console.error('There was an uncaught error', err);
-  //   // process.exit(1) //mandatory (as per the Node docs)
-  // });
-  
-  
-  //
-  // bytesIterable.pipe(writer);
-  // // Read the stream of bytes and add blocks to CAR
-  // // try {
-  // //   for await (const chunk of bytesIterable) {
-  // //     console.log('chunk', chunk);
-  // //     await writer.put({ cid, bytes: chunk });
-  // //   }
-  // // } catch (err) {
-  // //   console.error('Error while adding chunks to CAR:', err);
-  // //   return;
-  // // }
-  //
-  // // Finish writing
-  // await writer.close();
-  //
-  // // Get CAR as Uint8Array
-  // const carBytes = await out.toBytes();
-  //
-  // // Use carBytes as you like, for example, save as a file or send to a server
-  // console.log('CAR file successfully created:', carBytes);
